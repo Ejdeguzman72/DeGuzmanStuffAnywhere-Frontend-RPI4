79fdb49218dae8626122b25aeef9ca130b8bc34f
Ejdeguzman72 <ejdeguzman72@gmail.com>
2023-04-26 02:21:59 -0400

    more service url updates

--- src/services/AutoShopService.tsx
+++ src/services/AutoShopService.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Axios from 'axios';
 
 const getAllAutoShops = (params:any) => {
@@ -8,32 +9,20 @@ const getAllAutoShopsInfo = () => {
     return Axios.get('http://localhost:8080/app/auto-repair-shops/all');
 }
 
-const getAutoShopsByZip = (data:any) => {
-    return Axios.get(`http://localhost:8080/app/auto-repair-shops/all/search/zip`,data);
-}
-
 const getAutoShopById = (data:any) => {
     return Axios.get(`http://localhost:8080/app/auto-repair-shops/repair-shop/search/id`,data);
 }
 
-const searchAutoShopByName = (data:any) => {
-    return Axios.get(`http://localhost:8080/app/auto-repair-shops/repair-shop/search/name`,data);
-}
-
-const getAutoShopCount = () => {
-    return Axios.get(`http://localhost:8080/app/auto-repair-shops/count`);
-}
-
-const addAutoShop = (newData: any) => {
-    return Axios.post("http://localhost:8080/app/auto-repair-shops/add", newData);
+const addAutoShop = (newData:any) => {
+    return Axios.post("http://localhost:8080/app/auto-repair-shops/add-auto-shop", newData);
 }
 
 const updateAutoShop = (data:any) => {
-    return Axios.put(`http://localhost:8080/app/auto-repair-shops/update`,data);
+    return Axios.put(`http://localhost:8080/app/auto-repair-shops/repair-shop/update`,data);
 }
 
 const deleteAutoShop = (data:any) => {
-    return Axios.delete(`http://localhost:8080/app/auto-repair-shops/delete`);
+    return Axios.delete(`http://localhost:8080/app/auto-repair-shops/delete`,data);
 }
 
 const deleteAllShopInformation = () => {
@@ -48,8 +37,5 @@ export default {
     getAllAutoShopsInfo,
     getAutoShopById,
     updateAutoShop,
-    deleteAutoShop,
-    getAutoShopsByZip,
-    searchAutoShopByName,
-    getAutoShopCount
+    deleteAutoShop
 }
--- src/services/MusicService.js
+++ src/services/MusicService.js
@@ -8,32 +8,40 @@ const getAllMusicPagination = (params) => {
     return Axios.get('http://localhost:8080/app/music/all-songs', { params });
 }
 
-const getSongById = (song_id) => {
-    return Axios.get(`http://localhost:8080/app/music/song/${song_id}`);
+const getSongById = (data) => {
+    return Axios.get(`http://localhost:8080/app/music/song/id`,data);
 }
 
 const getSongByTitle = (title) => {
     return Axios.get(`http://localhost:8080/app/music/song/title/${title}`);
 }
 
-const getSongByArtist = (artist) => {
-    return Axios.get(`http://localhost:8080/app/music/song/artist/${artist}`);
+const getSongByArtist = (data) => {
+    return Axios.get(`http://localhost:8080/app/music/all/artist`,data);
+}
+
+const getSongsByGenre = (data) => {
+    return Axios.get(`http://localhost:8080/app/music/all/genre`,data)
+}
+
+const getSongCount = () => {
+    return Axios.get(`http://localhost:8080/app/music/count-of-songs`)
 }
 
 const addSongInformaton = (data) => {
     return Axios.post('http://localhost:8080/app/music/add-song-information', data);
 }
 
-const updateSongInformation = (song_id,data) => {
-    return Axios.put(`http://localhost:8080/app/music/song/${song_id, data}`);
+const updateSongInformation = (data) => {
+    return Axios.put(`http://localhost:8080/app/music/update`,data);
 }
 
-const deleteSongInformation = (song_id) => {
-    return Axios.delete(`http://localhost:8080/app/music/song/${song_id}`);
+const deleteSongInformation = (data) => {
+    return Axios.delete(`http://localhost:8080/app/music/delete`,data);
 }
 
 const deleteAllSongs = () => {
-    return Axios.delete('http://localhost:8080/app/music/delete-all-songs');
+    return Axios.delete('http://localhost:8080/app/music/delete-all');
 }
 
 export default {
